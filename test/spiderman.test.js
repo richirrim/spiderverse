f3e0deac077a967ca86dc4920726af6df03cefb6
richirrim <ricardoortega.dev@gmail.com>
2022-04-20 13:23:53 -0500

    Actualiza los test.

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('../app/spiderman')
+
 describe('Test Suite Dummy Description', () => {
   test('Case 1 Dummy', () => {
     const resultOfSomething = 1 + 2
@@ -8,8 +10,9 @@ describe('Test Suite Dummy Description', () => {
 describe('Unit Test para la clase Spiderman', () => {
   test('1) Crea un objeto', () => {
     const tobeyMaguire = new Spiderman('Spider-Man (2002)', 46, 'Tobey Maguire', 4, 'Marvel Entertainmen')
+
     expect(tobeyMaguire.name).toBe('Spider-Man (2002)')
-    expect(tobeyMaguire.edad).toBe(46)
+    expect(tobeyMaguire.age).toBe(46)
     expect(tobeyMaguire.actor).toBe('Tobey Maguire')
     expect(tobeyMaguire.movies).toBe(4)
     expect(tobeyMaguire.studio).toBe('Marvel Entertainmen')
