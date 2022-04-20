a3a1794715383961d8d2935cf154171e912e68a9
richirrim <ricardoortega.dev@gmail.com>
2022-04-20 12:49:16 -0500

    Crea prueba para la clase Spiderman.

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -4,3 +4,14 @@ describe('Test Suite Dummy Description', () => {
     expect(resultOfSomething).toBe(3)
   })
 })
+
+describe('Unit Test para la clase Spiderman', () => {
+  test('1) Crea un objeto', () => {
+    const tobeyMaguire = new Spiderman('Spider-Man (2002)', 46, 'Tobey Maguire', 4, 'Marvel Entertainmen')
+    expect(tobeyMaguire.name).toBe('Spider-Man (2002)')
+    expect(tobeyMaguire.edad).toBe(46)
+    expect(tobeyMaguire.actor).toBe('Tobey Maguire')
+    expect(tobeyMaguire.movies).toBe(4)
+    expect(tobeyMaguire.studio).toBe('Marvel Entertainmen')
+  })
+})
